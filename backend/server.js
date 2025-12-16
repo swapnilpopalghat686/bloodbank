07338a2fe2a7170e122f7cada4b7badfe19b248a
swapnilpopalghat686 <swapnilpopalghat44@gmail.com>
2025-12-16 12:34:00 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import donorRoutes from "./routes/donorRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import facilityRoutes from "./routes/facilityRoutes.js";
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173", // or 3000
+  credentials: true,
+}));
+
+
+// 🧩 Routes
+
+app.use("/api/auth", authRoutes);
+
+
+app.use("/api/donor", donorRoutes);
+
+app.use("/api/facility", facilityRoutes);
+
+app.use("/api/admin", adminRoutes);
+
+
+
+import bloodLabRoutes from "./routes/bloodLabRoutes.js";
+app.use("/api/blood-lab", bloodLabRoutes);
+
+
+import hospitalRoutes from "./routes/hospitalRoutes.js";
+app.use("/api/hospital", hospitalRoutes);
+
+
+// 🗄️ DB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log("MongoDB Error ❌", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT} 🚀`));
